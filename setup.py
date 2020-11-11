3e82c67aac7501a95012df5c3cfa30f7d3616461
Kyoungseoun Chung <kchung@student.ethz.ch>
2020-11-11 12:48:30 +0100

    MES: working on packaging...

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rise2pdf", # Replace with your own username
+    version="0.0.1",
+    author="Kyoungseoun Chung",
+    author_email="kchung@student.ethz.ch",
+    description="Convert RISE slides to pdf using decktape",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
